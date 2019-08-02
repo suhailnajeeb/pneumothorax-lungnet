eb3cafcef74e3840a5b013727d6cb39d0062f7ee
suhailnajeeb <suhailnajeeb19@gmail.com>
2019-08-02 18:18:32 +0600

    sync

--- evaluateAll.py
+++ evaluateAll.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 from mask_functions import mask2rle
 
-def predictImage(f,IMG_SIZE = 256):
+def predictImage(f,IMG_SIZE = 512):
     ORIG_DIM = 1024
     resize = (IMG_SIZE, IMG_SIZE)
 
@@ -21,34 +21,23 @@ def predictImage(f,IMG_SIZE = 256):
     img = np.expand_dims(img, axis = 2)
     img = np.expand_dims(img, axis = 0)
     mask = model.predict(img)
-
-    threshold = 0.5
-
-    mask = mask.squeeze()
-    #mask = mask.reshape(512,512)
-    mask = cv2.resize(mask, (1024,1024))
-    mask = mask > threshold
     
-    if mask.sum()<1024*2:
-        mask[:] = 0
-    mask = (mask.T*255).astype(np.uint8)  
+    mask = mask.reshape(512,512)
+    mask = cv2.resize(mask, (1024,1024))
+    mask = mask.round()
+    mask = mask*255
+
     rle = mask2rle(mask, 1024, 1024)
-
-    #mask = mask.round()
-    #mask = mask*255
-
-    #rle = mask2rle(mask, 1024, 1024)
     return rle
 
-bestModelPath = '..\\out\\weights\\LungNet_25-loss--0.447.hdf5'
+bestModelPath = '..\\out\\weights\\best.hdf5'
 
-#model = load_model(bestModelPath)
 model = LungNet001a()
 model.load_weights(bestModelPath)
 
 PATH_VAL = '..\\Data\\dicom-images-test\\'
 CSVFILE = '..\\Data\\kanvari.csv'
-SUBCSV = '..\\out\\submission_256_true.csv'
+SUBCSV = '..\\out\\submission.csv'
 
 val = glob2.glob(os.path.join(PATH_VAL,'**/*.dcm'))
 
@@ -62,18 +51,8 @@ idx = 0
 
 for f in val:
     id = f.split('\\')[-1][:-4]
-    x = df.loc[df.ImageId == id]
-    try:
-        x = x.iloc[0]['EncodedPixels']
-        if (x == -1):
-            print(id + " : False, Skipping " + str(idx) + " of " + str(n))
-            rle = '-1'
-        else:
-            print(id + " : True, Predicting " + str(idx) + " of " + str(n))
-            rle = predictImage(f)
-    except:
-        print(id + " : True, Predicting " + str(idx) + " of " + str(n))
-        rle = predictImage(f)
+    print(id + "Predicting " + str(idx) + " of " + str(n))
+    rle = predictImage(f)
     ids.append(id)
     rles.append(rle)
     idx = idx + 1
